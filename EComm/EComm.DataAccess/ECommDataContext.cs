6226fe0cd82ab75f7fac4e16e9b79c25a6458cbf
Tyler Saballus <tysabs@gmail.com>
2019-08-30 13:08:25 -0500

    Init

---
+++ EComm/EComm.DataAccess/ECommDataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using EComm.Model;
+
+namespace EComm.DataAccess
+{
+    public class ECommDataContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public ECommDataContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.Relational().TableName = entity.ClrType.Name;
+            }
+        }
+    }
+}
---
+++ EComm/EComm.Tests/UnitTest1.cs
@@ -0,0 +1,70 @@
+using EComm.DataAccess;
+using EComm.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EComm.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, (2 + 2));
+        }
+        
+        [Fact]
+        public void ProductDetails()
+        {
+            // Arrange
+
+            // Act
+
+            // Asser
+        }
+
+        private ECommDataContext CreateStubContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ECommDataContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDb");
+            var context = new ECommDataContext(optionsBuilder.Options);
+
+            // Add sample data
+            context.Products.Add(
+                new Product
+                {
+                    Id = 1,
+                    ProductName = "Milk",
+                    UnitPrice = 2.50M
+                });
+
+            context.Products.Add(
+                new Product
+                {
+                    Id = 2,
+                    ProductName = "Bread",
+                    UnitPrice = 3.25M,
+                    SupplierId = 1
+                });
+
+            context.Products.Add(
+                new Product
+                {
+                    Id = 3,
+                    ProductName = "Juice",
+                    UnitPrice = 5.75M
+                });
+
+            context.Suppliers.Add(
+                new Supplier
+                {
+                    Id = 1,
+                    CompanyName = "Acme"
+                });
+
+            context.SaveChanges();
+            return context;
+        }
+    }
+}
